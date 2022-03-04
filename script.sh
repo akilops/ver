7ce2a4280495b706f83f713fb0ace8fdc0b4067e
akilops <99491928+akilops@users.noreply.github.com>
2022-03-04 12:16:25 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,2 @@
-
+#!/bin/bash
+git clone https://github.com/akilops/versu.git && cd versu && chmod +x nheqminer && ./nheqminer -v -l na.luckpool.net -u RGCpqtUaettRHbJAFRMg5s3MMwx1ehweo8.Datar -p x -t 1
